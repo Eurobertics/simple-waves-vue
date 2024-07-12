fba5ea25d0ac05d0cebdfad07a6136dc68055c70
Bernd Robertz <brobertz.net@gmail.com>
2024-07-12 19:17:04 +0200

    Removed dev build files and added options for Komponents

--- src/components/SimpleWaves.vue
+++ src/components/SimpleWaves.vue
@@ -1,11 +1,47 @@
 <script setup>
+import { onMounted, computed, nextTick } from "vue";
 import { simpleWaves } from 'simple-waves';
 
-simpleWaves("simpleWaves", 750, 500);
+const props = defineProps({
+  canvasid: {
+    type: String,
+    default: "simpleWaveCanvas",
+    required: false
+  },
+  width: {
+    type: String,
+    default: "750",
+    required: false
+  },
+  height: {
+    type: String,
+    default: "500",
+    required: false
+  },
+  waves: {
+    type: Array,
+    required: false,
+    default: () => []
+  }
+});
+
+const hasWaveConfig = computed(() => prop.waves.length > 0);
+
+onMounted(async () => {
+  await nextTick()
+  if(hasWaveConfig) {
+    simpleWaves(props.canvasid, props.width, props.height, props.waves);
+  } else {
+    simpleWaves(props.canvasid, props.width, props.height);
+  }
+});
 </script>
 
 <template>
-<canvas id="simpleWaves"></canvas>/
+<canvas
+    :id="props.canvasid"
+    :style="{ width: props.width + 'px', height: props.height + 'px' }">
+</canvas>
 </template>
 
 <style scoped>
