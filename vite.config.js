0d703ff7fc8101130edd61b8ce3a96357dbf2797
Bernd Robertz <brobertz.net@gmail.com>
2025-02-27 23:01:54 +0100

    Updated deprecated package in package.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { terser } from 'rollup-plugin-terser';
+import terser from "@rollup/plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
